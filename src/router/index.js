11f148d9a85b0b5b64182f5af87d0a357d7ed178
ChenSir <1474591391@qq.com>
2022-04-05 21:38:08 +0800

    初始化路由路径

---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const routes = [
+  { path: '/', redirect: '/index' },
+  { path: '/index', name: 'index', component: () => import('@/views/index/index.vue') },
+  { path: '/playlist', name: 'playlist', component: () => import('@/views/playlist/playlist.vue') },
+];
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+export default router;
