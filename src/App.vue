eb3a9cdf3959128ac873d7609accbdf9fce90b57
ChenSir <1474591391@qq.com>
2022-04-05 21:37:00 +0800

    初始化布局结构

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside width="100px"><SideBar /></el-aside>
+      <el-main> <router-view></router-view></el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script setup>
+import SideBar from '@/layout/Sidebar.vue';
+</script>
---
+++ src/layout/Sidebar.vue
@@ -0,0 +1,46 @@
+<template>
+  <el-row class="tac">
+    <el-col>
+      <el-menu default-active="1">
+        <el-menu-item
+          v-for="(item, index) in menuList"
+          :key="item.path"
+          :index="`${index}`"
+          @click="menuClick(item.path)"
+        >
+          <span>{{ item.name }}</span>
+        </el-menu-item>
+      </el-menu>
+    </el-col>
+  </el-row>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+import { reactive } from 'vue';
+const menuList = reactive([
+  {
+    name: '首页',
+    path: '/index',
+  },
+  {
+    name: '歌单',
+    path: 'playlist',
+  },
+]);
+const router = useRouter();
+const menuClick = path => {
+  router.push({
+    path,
+  });
+};
+
+const handleOpen = (key, ketPath) => {
+  console.log(key, ketPath);
+};
+const handleClose = (key, keyPath) => {
+  console.log(key, keyPath);
+};
+</script>
+
+<style></style>
