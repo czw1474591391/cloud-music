4a6c33c060a11423ad88aefcd69c66427e3c5458
chenziwu <1474591391@qq.com>
2022-04-07 18:01:32 +0800

    增加头部组件

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <el-container>
-    <el-header>Header</el-header>
+    <el-header><Header /></el-header>
     <el-container>
       <el-aside width="200px"><SideBar /></el-aside>
-      <el-main> <router-view></router-view></el-main>
+      <el-main> <router-view /></el-main>
     </el-container>
   </el-container>
 </template>
 
 <script setup>
 import SideBar from '@/layout/Sidebar.vue';
+import Header from '@/layout/Header.vue';
 </script>
 <style>
 .icon {
