8f0e47156165a8677b53895c39de89ee0cb96988
chenziwu <1474591391@qq.com>
2022-04-07 13:15:55 +0800

    完善路由页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,35 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
-  { path: '/', redirect: '/index' },
-  { path: '/index', name: 'index', component: () => import('@/views/index/index.vue') },
-  { path: '/playlist', name: 'playlist', component: () => import('@/views/playlist/playlist.vue') },
+  {
+    path: '/',
+    redirect: '/index',
+  },
+  {
+    path: '/index',
+    name: 'index',
+    component: () => import('@/views/index/index.vue'),
+  },
+  {
+    path: '/playlist',
+    name: 'playlist',
+    component: () => import('@/views/playlist/playlist.vue'),
+  },
+  {
+    path: '/singer',
+    name: 'singer',
+    component: () => import('@/views/singer/singer.vue'),
+  },
+  {
+    path: '/myMusic',
+    name: 'myMusic',
+    component: () => import('@/views/myMusic/myMusic.vue'),
+  },
 ];
+
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
 export default router;
