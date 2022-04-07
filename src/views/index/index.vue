f21aac92077860bc0ae300668d91b4da19af3c15
chenziwu <1474591391@qq.com>
2022-04-07 18:01:50 +0800

    轮播图

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,6 +1,22 @@
-<template>123456</template>
+<template>
+  <Banners :List="bannerList"></Banners>
+</template>
 
-<script setup></script>
+<script setup>
+import { getCurrentInstance, onBeforeMount, reactive, ref, watch } from 'vue';
+import { Banners } from '@/components';
 
-<style></style>
-    
+const { proxy } = getCurrentInstance();
+onBeforeMount(() => {
+  getBanners();
+});
+const bannerList = ref([]);
+
+// 获取轮播图数据
+const getBanners = async () => {
+  const data = await proxy.$http.banner(2);
+  bannerList.value = data.banners || [];
+};
+</script>
+
+<style lang="scss" scoped></style>
