cecb801fa82ddf6e90b290f69244d6f2a175181f
chenziwu <1474591391@qq.com>
2022-04-09 17:52:33 +0800

    更新阿里图标库引用

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import * as apis from '@/apis/http';
 import router from '@/router';
-import '@/utils/iconfont/iconfont.js';
+import 'https://at.alicdn.com/t/font_2871490_x6fbkisuymm.js?spm=a313x.7781069.1998910419.117&file=font_2871490_x6fbkisuymm.js';
 
 const app = createApp(App);
 // 全局挂载http请求库
