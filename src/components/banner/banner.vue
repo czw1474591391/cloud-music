6a92468c695a954cfa07f60c4ade10380c0c4638
chenziwu <1474591391@qq.com>
2022-04-09 17:54:10 +0800

    增加骨架屏效果

--- src/components/banner/banner.vue
+++ src/components/banner/banner.vue
@@ -1,20 +1,28 @@
 <template>
-  <swiper
-    :modules="[Navigation, Pagination, Autoplay]"
-    :slides-per-view="4"
-    :space-between="30"
-    :autoplay="{ delay: 1500 }"
-    :pagination="{ clickable: true }"
-    ref="mySwiper"
-    class="banner_wrap"
-  >
-    <swiper-slide v-for="item in List" :key="item.imageUrl">
-      <el-image :src="item.imageUrl" :alt="item.typeTitle" class="banner_img"> </el-image>
-    </swiper-slide>
-  </swiper>
+  <div class="banner_wrap">
+    <el-skeleton :loading="loading" animated>
+      <template #template>
+        <el-skeleton-item variant="image" v-for="item in 4" :key="item" class="skeleton-img" />
+      </template>
+      <template #default>
+        <swiper
+          :modules="[Navigation, Pagination, Autoplay]"
+          :slides-per-view="4"
+          :space-between="30"
+          :autoplay="{ delay: 1500 }"
+          :pagination="{ clickable: true }"
+          class="swiper"
+        >
+          <swiper-slide v-for="item in List" :key="item.imageUrl">
+            <el-image :src="item.imageUrl" :alt="item.typeTitle" class="banner_img"> </el-image>
+          </swiper-slide>
+        </swiper>
+      </template>
+    </el-skeleton>
+  </div>
 </template>
 <script setup>
-import { defineProps } from 'vue';
+import { defineProps, ref, watch } from 'vue';
 import { Navigation, Pagination, Autoplay } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/vue';
 import 'swiper/css';
@@ -26,10 +34,27 @@ const props = defineProps({
     default: [],
   },
 });
+const loading = ref(true);
+// 监听异步数据，返回则关闭骨架屏loading
+watch(
+  () => props.List,
+  () => (loading.value = false)
+);
 </script>
 
 <style lang="scss" scoped>
 .banner_wrap {
-  padding-bottom: 30px;
+  .el-skeleton {
+    display: flex;
+    justify-content: space-between;
+    .skeleton-img {
+      flex: 1;
+      margin-right: 30px;
+      height: 140px;
+    }
+  }
+  .swiper {
+    padding-bottom: 30px;
+  }
 }
 </style>
--- src/components/hot-playlist/hot-playlist.vue
+++ src/components/hot-playlist/hot-playlist.vue
@@ -11,18 +11,41 @@
       </div>
     </div>
     <div class="hot-items">
-      <div v-for="item in hotPlayList" :key="item.id">
-        <!-- <img :src="item.coverImgUrl" alt="" /> -->
-        <el-image :src="item.coverImgUrl" lazy></el-image>
-        <p>{{ item.name }}</p>
-      </div>
+      <el-skeleton :loading="loading" animated :throttle="1000">
+        <template #template>
+          <div v-for="item in 6" :key="item" class="hot-skeleton-warp">
+            <el-skeleton-item
+              variant="image"
+              style="width: 227px; height: 227px; margin-bottom: 20px"
+            ></el-skeleton-item>
+            <el-skeleton-item variant="text" style="width: 100px" />
+            <el-skeleton-item variant="text" style="width: 200px" />
+          </div>
+        </template>
+        <template #default>
+          <div v-for="item in hotPlayList" :key="item.id" @click="toDetail(item.id)">
+            <el-image :src="item.coverImgUrl" lazy></el-image>
+            <p>{{ item.name }}</p>
+            <span v-for="item2 in item.tags" :key="item2" class="tags">{{ item2 }}</span>
+            <div class="playCount">
+              <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-play-num"></use>
+              </svg>
+              <span>{{ computed_Count(item.playCount) }} / </span>
+              <span>{{ item.trackCount }}首</span>
+            </div>
+          </div>
+        </template>
+      </el-skeleton>
     </div>
   </el-card>
 </template>
 
 <script setup>
-import { getCurrentInstance, reactive, onBeforeMount, ref } from 'vue';
+import { getCurrentInstance, reactive, onBeforeMount, ref, computed } from 'vue';
+import { useRouter } from 'vue-router';
 const { proxy } = getCurrentInstance();
+const router = useRouter();
 const hotMenu = reactive([
   {
     id: 0,
@@ -46,29 +69,53 @@ const hotMenu = reactive([
   },
   {
     id: 4,
-    name: '古风',
+    name: '欧美',
     class: '',
   },
 ]);
-let hotPlayList = ref([]);
-onBeforeMount(() => {
-  getHotPlatList();
-});
+const hotPlayList = ref([]);
+const loading = ref(true);
+onBeforeMount(() => getHotPlatList());
+
 const getHotPlatList = async (cat = '流行') => {
   const data = await proxy.$http.hotPlayList(6, cat);
   hotPlayList.value = data.playlists;
+  loading.value = false;
 };
-
 // 过滤菜单class属性
 const filterClass = e => {
   hotMenu.map(item => {
     item.class = item.id === e.id ? 'hotMenuActive' : '';
   });
+  loading.value = true; // 每次切换热门歌单 将骨架屏loading打开
   getHotPlatList(e.name);
+};
+
+// 计算播放量
+const computed_Count = computed(() => num => (num % 1000) + '万');
+
+const toDetail = id => {
+  // 传递参数使用query的话，指定path或者name都行，但使用params的话，只能使用name指定
+  router.push({
+    // path: '/playlist/detail',
+    name: 'playlist-detail',
+    params: {
+      id,
+    },
+  });
 };
 </script>
 
 <style lang="scss" scoped>
+.icon {
+  width: 20px;
+  height: 20px;
+  vertical-align: -0.4em;
+  fill: currentColor;
+  overflow: hidden;
+  margin-right: 5px;
+  color: white;
+}
 .hot-card-container {
   border-radius: 15px;
   .hot-title {
@@ -93,11 +140,36 @@ const filterClass = e => {
   .hot-items {
     display: flex;
     justify-content: space-around;
-    div {
+    > div {
+      position: relative;
       margin: 20px 40px 0 0;
-      text-align: center;
       .el-image {
         margin: 0;
+      }
+      p {
+        font-size: 16px;
+      }
+      .tags {
+        font-size: 14px;
+        margin-right: 10px;
+      }
+      .tags::before {
+        content: '#';
+        color: rgba(255, 0, 0, 0.836);
+      }
+      .playCount {
+        position: absolute;
+        top: 10px;
+        right: 8px;
+        color: white;
+        font-size: 12px;
+      }
+    }
+    .el-skeleton {
+      display: flex;
+      margin: 10px 0;
+      .hot-skeleton-warp {
+        flex: 1;
       }
     }
   }
