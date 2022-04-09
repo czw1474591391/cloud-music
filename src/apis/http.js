eb10f00062abbe20927ff02f9148f7e74f08530b
chenziwu <1474591391@qq.com>
2022-04-09 17:52:47 +0800

    增加歌单详情接口

--- src/apis/http.js
+++ src/apis/http.js
@@ -1,14 +1,17 @@
 import api from '@/utils/axios';
 const { get, post } = api;
 
-const search = ({ keywords = '' }) => get(`/search?keywords=${keywords}`, {});
+const search = ({ keywords = '' }) => get(`/search?keywords=${keywords}`);
 
 // 获取轮播图，调用例子/banner?type=2,
 // 0:pc 1:android 2:iphone 3: ipad
-const banner = (num = 0) => get(`/banner?type=${num}`, {});
+const banner = (num = 0) => get(`/banner?type=${num}`);
 
 // 获取热门歌单 调用例子/top/playlist?limit=10&order=new
 // cat:标签 '古风' '民谣' 等等 limit:取出歌单数量 默认50
 const hotPlayList = (limit = 6, cat = '流行') => get(`/top/playlist?limit=${limit}&cat=${cat}`);
 
-export { search, banner, hotPlayList };
+// 获取歌单详情 调用例子:/playlist/detail?id=
+const playListDetail = (id = 6928102820) => get(`/playlist/detail?id=${id}`);
+
+export { search, banner, hotPlayList, playListDetail };
