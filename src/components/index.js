e62eb8e08d379103ebf0a9ddd61b2f87447b4745
chenziwu <1474591391@qq.com>
2022-04-09 17:53:20 +0800

    增加导出歌单详情头部

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import Banners from './banner/banner.vue';
 import HotPlayList from './hot-playlist/hot-playlist.vue';
+import detailHeader from './playlist-detail-header/playlist-detail-header.vue';
 
-export { Banners, HotPlayList };
+export { Banners, HotPlayList, detailHeader };
