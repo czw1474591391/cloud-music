cbebed7a3cb1418c636b7bec2274be2365dced46
chenziwu <1474591391@qq.com>
2022-04-16 17:56:36 +0800

    增加rem布局方案

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import * as apis from '@/apis/http';
 import router from '@/router';
+import '@/utils/rem.js'; // 引入rem布局方案
 import 'https://at.alicdn.com/t/font_2871490_x6fbkisuymm.js?spm=a313x.7781069.1998910419.117&file=font_2871490_x6fbkisuymm.js';
 
 const app = createApp(App);
