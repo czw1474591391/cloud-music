f9ae2022b8d957467d167448e82a8b35403acfed
chenziwu <1474591391@qq.com>
2022-04-18 17:57:51 +0800

    更改全局尺寸

--- src/utils/rem.js
+++ src/utils/rem.js
@@ -1,8 +1,7 @@
 function setRem() {
-  var whdef = 50 / 1920; // 表示1920的设计图,使用100PX的默认值
+  var whdef = 15 / 1920; // 表示1920的设计图,使用100PX的默认值
   var bodyWidth = document.body.clientWidth; // 当前窗口的宽度
   var rem = bodyWidth * whdef; // 以默认比例值乘以当前窗口宽度,得到该宽度下的相应FONT-SIZE值
-  console.log(rem);
   document.getElementsByTagName('html')[0].style.fontSize = rem + 'px';
 }
 window.addEventListener('load', setRem);
