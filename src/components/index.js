f0326a6f1fa15405519ecc3709c64380a0051038
chenziwu <1474591391@qq.com>
2022-04-18 17:57:23 +0800

    导出组件重命名

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
-import Banners from './banner/banner.vue';
-import HotPlayList from './hot-playlist/hot-playlist.vue';
-import detailHeader from './playlist-detail-header/playlist-detail-header.vue';
-import newsDiscShelves from './new-disc-shelves/new-disc-shelves.vue';
+import Banners from './banner/banner.vue'; // 轮播图
+import HotPlayList from './hot-playlist/hot-playlist.vue'; // 热门推荐
+import NewsDiscShelves from './new-disc-shelves/new-disc-shelves.vue'; // 新碟上架
+import TopList from './top-list/top-list.vue'; // 热门榜单
+import DetailHeader from './playlist-detail-header/playlist-detail-header.vue'; // 详情头部
 
-export { Banners, HotPlayList, detailHeader, newsDiscShelves };
+export { Banners, HotPlayList, DetailHeader, NewsDiscShelves, TopList };
