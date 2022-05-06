50c0f67fc561fc3909fbbb5d87df26fc6da69f99
chenziwu <1474591391@qq.com>
2022-05-06 10:30:55 +0800

    更改逻辑写法

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -8,48 +8,51 @@
 </template>
 
 <script setup>
-import { getCurrentInstance, onBeforeMount, ref } from 'vue';
+import { banner, newDiscShelves, allTopList, playListDetail, hotPlayList } from '@/apis/http';
+import { onBeforeMount, ref } from 'vue';
 import { newsDisc } from '@/utils/enums.js';
 import { Banners, HotPlayList, NewsDiscShelves, TopList } from '@/components';
 
-const { proxy } = getCurrentInstance();
-const { banner, newDiscShelves, allTopList, playListDetail, hotPlayList } = proxy.$http; //解构api方法
+// const { proxy } = getCurrentInstance();
+// const { banner, newDiscShelves, allTopList, playListDetail, hotPlayList } = proxy.$http; //解构api方法
 onBeforeMount(() => {
   getBanners();
   getNewDisc();
   getAllList();
   getHotPlatList();
 });
-const bannerList = ref([]); // 轮播图数据
-const NewDiscList = ref([]); // 新碟上架数据
-const topList = ref([]); // 榜单数据
-const hotList = ref([]); // 热门推荐数据
 
 // 获取轮播图数据
+const bannerList = ref([]);
 const getBanners = async () => {
   const data = await banner();
   bannerList.value = data?.banners;
 };
+
 // 获取热门推荐
+const hotList = ref([]);
 const getHotPlatList = async cat => {
   const data = await hotPlayList({ cat });
   hotList.value = data?.playlists;
 };
 
 // 获取新碟上架数据
+const NewDiscList = ref([]);
 const getNewDisc = async area => {
-  const data = await newDiscShelves({ area: newsDisc[area] });
-  NewDiscList.value = data?.monthData.slice(0, 12);
+  const { monthData, weekData } = await newDiscShelves({ area: newsDisc[area] });
+  NewDiscList.value = monthData.length === 0 ? weekData.slice(0, 12) : monthData.slice(0, 12);
+  // 如果不存在月数据那就取周数据
 };
 
 // 获取所有榜单
+const topList = ref([]);
 const getAllList = async () => {
   const data = await allTopList();
   const list = [];
-  for (const item of data.list.slice(0, 4)) {
+  for (const item of data?.list.slice(0, 4)) {
     const result = await playListDetail(item.id);
-    let { tracks } = result.playlist;
-    list.push({ ...result.playlist, tracks: tracks.slice(0, 6) });
+    let { tracks } = result?.playlist;
+    list.push({ ...result?.playlist, tracks: tracks.slice(0, 6) });
     // 单独截断歌曲数组
   } // 获取热门歌单id  然后请求最新的榜单
   topList.value = list;
